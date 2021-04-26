7e48cb176d265441cf90fd245bd63e691c6f452d
ajramos <tatto2k@gmail.com>
2021-04-26 18:54:36 +0200

    New backup 2021-04-26 18:54

--- backup.sh
+++ backup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # copy other dot files 
-cp  $HOME/{.zshrc,.vimrc,.tmux.conf,.robomongorc.js} .
-cp  $HOME/.gitconfig .
-cp  $HOME/.config/nvim/init.vim ./.config/nvim/
-cp  $HOME/.config/kitty/kitty.conf ./.config/kitty/
+cp -v $HOME/{.zshrc,.vimrc,.tmux.conf,.robomongorc.js} .
+cp -v $HOME/.gitconfig .
+cp -v $HOME/.config/nvim/init.vim ./.config/nvim/
+cp -v $HOME/.config/kitty/kitty.conf ./.config/kitty/
+
+#Copy fonts
+cp -vr $HOME/.local/share/fonts ./.local/share/fonts
 
 # push to Github
 git add -u;
-git commit -m "New backup `date +'%Y-%m-%d'`";
+git commit -m "New backup `date +'%Y-%m-%d %H:%M'`";
 git push origin main;
